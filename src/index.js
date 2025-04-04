6575e4e1ead1dbbd5e53b9c9062d69b2a23234ec
SibongileT88 <sibongilet@beskbridge.com>
2025-04-04 13:46:31 +0200

    added Javascript and imported moment.js and moment.tz

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+function updateTime() {
+  let johannesburgElement = document.querySelector("#johannesburg");
+  let johannesburgDateElement = johannesburgElement.querySelector(".date");
+  let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+  let johannesburgTime = moment().tz("Africa/Johannesburg");
 
+  johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+  johannesburgTimeElement.innerHTML = johannesburgTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
+
+  let dubaiElement = document.querySelector("#dubai");
+  let dubaiDateElement = dubaiElement.querySelector(".date");
+  let dubaiTimeElement = dubaiElement.querySelector(".time");
+  let dubaiTime = moment().tz("Asia/Dubai");
+
+  dubaiDateElement.innerHTML = dubaiTime.format("MMMM Do YYYY");
+  dubaiTimeElement.innerHTML = dubaiTime.format("h:mm:ss [<small>]A[<small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
